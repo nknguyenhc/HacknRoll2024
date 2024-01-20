f722f7864fa973c0c2ca1c8ae2be07f8d88c0b04
Nguyen Cao Duy <nguyen.cao.duy.0303@gmail.com>
2024-01-20 12:12:54 +0800

    Init front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Main from "./components/Main";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#2272FF',
+    },
+    secondary: {
+      main: '#FFFFFF',
+    },
+  },
+});
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Main />
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/components/Main.js
@@ -0,0 +1,113 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import InputAdornment from "@mui/material/InputAdornment";
+import api from "../api";
+
+export default function Album() {
+  const [url, setUrl] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const [isUrlSubmitted, setIsUrlSubmitted] = React.useState(false);
+
+  const handleUrlChange = (e) => {
+    setUrl(e.target.value);
+  };
+  const handleFindVideo = () => {
+    api.get("/title", { params: { url: url } }).then((res) => {
+      console.log(res.data);
+    });
+  };
+  const handleGenerateVideo = () => {
+    api.get("/generate", { params: { url: url } }).then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  return (
+    <main>
+      <Box
+        sx={{
+          bgcolor: "background.paper",
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h4"
+            align="center"
+            color="primary"
+            fontWeight="800"
+            gutterBottom
+          >
+            Generate Your Own Music Video
+          </Typography>
+          <Typography
+            variant="h6"
+            align="center"
+            color="text.secondary"
+            paragraph
+          >
+            Parodies is an AI-Powered platform that generates a music video for
+            any youtube video of your choice, at your command.
+          </Typography>
+          <Stack
+            sx={{ pt: 4 }}
+            direction="column"
+            spacing={2}
+            justifyContent="center"
+          >
+            <TextField
+              variant="outlined"
+              fullWidth
+              value={url}
+              onChange={handleUrlChange}
+              disabled={isUrlSubmitted}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <YouTubeIcon />
+                  </InputAdornment>
+                ),
+                sx: {
+                  borderRadius: "10px",
+                },
+              }}
+            />
+            {isUrlSubmitted && (
+              <TextField
+                variant="outlined"
+                fullWidth
+                value={title}
+                disabled
+                InputProps={{
+                  sx: {
+                    borderRadius: "10px",
+                  },
+                }}
+              />
+            )}
+            {isUrlSubmitted ? (
+              <Button variant="contained" onClick={handleGenerateVideo}>
+                Generate Video
+              </Button>
+            ) : (
+              <Button variant="contained" onClick={handleFindVideo}>
+                Find Video
+              </Button>
+            )}
+          </Stack>
+        </Container>
+      </Box>
+    </main>
+  );
+}
