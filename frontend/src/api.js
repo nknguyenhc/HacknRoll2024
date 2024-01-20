31ad54cd686b205f734302de0f424b51e792ab83
Nguyen Cao Duy <nguyen.cao.duy.0303@gmail.com>
2024-01-20 17:41:13 +0800

    Fix api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: 'http://localhost:8000',
-});
+const instance = axios.create();
 
 export default instance;
---
+++ frontend/src/components/VideoDialog.js
@@ -0,0 +1,38 @@
+import api from "../api";
+import { Dialog, DialogContent, Typography } from "@mui/material";
+import ReactPlayer from "react-player";
+
+function VideoDialog({ open, handleClose, title, vidId }) {
+  return (
+    <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+      <DialogContent
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Typography
+          variant="h6"
+          align="center"
+          color="primary"
+          sx={{
+            borderRadius: "10px",
+            textAlign: "center",
+            fontWeight: "800",
+          }}
+        >
+          Video Title: {title}
+        </Typography>
+        <ReactPlayer
+            url={`content/${vidId}`}
+            controls={true}
+            playing={true}
+          />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default VideoDialog;
